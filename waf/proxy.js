22f7a0ced6efb0e5fd3e0533a19d4e7eb6cc08e9
haku-noir <hakunoir.00@gmail.com>
2019-11-08 16:31:00 +0900

    add: GETの情報をコンソールに表示する機能の追加

--- waf/proxy.js
+++ waf/proxy.js
@@ -1,11 +1,14 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
+var url = require('url');
 var rd = require('./remote_data');
 
+var proxy = httpProxy.createProxyServer();
 var port = process.env.PORT || 80;
 
 function requestHandler(req, res) {
-  var proxy = httpProxy.createProxyServer();
+  url_parse = url.parse(req.url, true);
+  console.log(url_parse);
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   proxy.web(req, res, {target: rd.server});
