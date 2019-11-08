fad08b88a2cda2f0a7a78884d60accc28580ff69
haku-noir <hakunoir.00@gmail.com>
2019-11-08 17:03:15 +0900

    add: ログを管理するloggerモジュールの追加

---
+++ waf/my_modules/logger.js
@@ -0,0 +1,21 @@
+var url = require('url');
+
+const printURL = (req) => {
+  console.log(`${req.method} ${req.url}`);
+}
+
+const printPathname = (req) => {
+  let url_parse = url.parse(req.url, true);
+  console.log(`${req.method} ${url_parse.pathname}`);
+}
+
+const printQuery = (req) => {
+  let url_parse = url.parse(req.url, true);
+  console.log(`${req.method} ${JSON.stringify(url_parse.query)}`);
+}
+
+module.exports = {
+  printURL,
+  printPathname,
+  printQuery
+};
--- waf/proxy.js
+++ waf/proxy.js
@@ -1,14 +1,14 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
-var url = require('url');
+var logger = require('./my_modules/logger');
 var rd = require('./remote_data');
 
 var proxy = httpProxy.createProxyServer();
 var port = process.env.PORT || 80;
 
 function requestHandler(req, res) {
-  url_parse = url.parse(req.url, true);
-  console.log(url_parse);
+  logger.printPathname(req);
+  logger.printQuery(req);
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   proxy.web(req, res, {target: rd.server});
