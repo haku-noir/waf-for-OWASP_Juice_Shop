32d483789223cb3a581a0e07562391c9cf962cb9
haku-noir <hakunoir.00@gmail.com>
2019-11-09 17:46:37 +0900

    add: 攻撃を検知した場合はリクエストを防ぐ機能の追加

--- waf/proxy.js
+++ waf/proxy.js
@@ -7,13 +7,18 @@ var rd = require('./remote_data');
 var proxy = httpProxy.createProxyServer();
 var port = process.env.PORT || 80;
 
-function requestHandler(req, res) {
+const requestHandler = (req, res) => {
   logger.printPathname(req);
   logger.printQuery(req);
-  console.log(checker.check(req));
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  proxy.web(req, res, {target: rd.server});
+  if(checker.check(req)){
+    res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.write('Block');
+    res.end();
+  }else{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    proxy.web(req, res, {target: rd.server});
+  }
 }
 
 http.createServer(requestHandler).listen(port);
