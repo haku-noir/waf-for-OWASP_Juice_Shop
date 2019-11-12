53d93e3dfa1ec716836969b3c9c293a2cee0dce8
haku-noir <hakunoir.00@gmail.com>
2019-11-12 23:14:21 +0900

    change: writeJSONがPromiseを返すように変更

--- waf/datas/csv-to-js.js
+++ waf/datas/csv-to-js.js
@@ -4,18 +4,18 @@ const csv = require('csv');
 const readCSV = (file_path) => new Promise((resolve) => {
   fs.createReadStream(file_path)
     .pipe(csv.parse({columns: true}, (err, data) => {
+      if(err) console.log(err);
       resolve(data);
     }));
 });
 
-const writeJSON = (file_path, data) => {
+const writeJSON = (file_path, data) => new Promise((resolve) => {
   fs.writeFile(file_path, JSON.stringify(data, null, '  '), (err) => {
     if(err) console.log(err);
+    resolve(data);
   });
-};
+});
 
 Promise.resolve()
   .then(() => readCSV(__dirname + '/query-check.csv'))
-  .then((data) => {
-    writeJSON(__dirname + '/query-check.json', data)
-  });
+  .then((data) => writeJSON(__dirname + '/query-check.json', data));
