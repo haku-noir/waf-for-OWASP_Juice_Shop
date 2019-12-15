01eafd1d20c4bf85074bb619216ecb5890cb4848
haku-noir <hakunoir.00@gmail.com>
2019-12-15 23:10:06 +0900

    change: checkerをPromise化

--- waf/my_modules/checker.js
+++ waf/my_modules/checker.js
@@ -1,41 +1,33 @@
 var url = require('url');
 
-const queryChecker = (req) => {
+const queryChecker = req => new Promise((resolve, reject) => {
   let check_items = require('../datas/query-check.json');
   let url_parse = url.parse(req.url, true);
 
   let attack = false;
-  check_items.forEach((check_item) => {
+  check_items.some(check_item => {
     if(url_parse.pathname === check_item.pathname && url_parse.query[check_item.query]
         && url_parse.query[check_item.query].indexOf(check_item.value) !== -1){
       attack = true;
     }
 
+    if(attack) reject();
+
     return attack;
   });
 
-  return attack;
-}
+  resolve();
+});
 
 const checkers = {
   queryChecker
 };
 
-const check = (req) => {
-  let attack = false;
-
-  Object.keys(checkers).some((key) => {
-    let checker = checkers[key];
-
-    if(checker(req)){
-      attack = true;
-    }
-
-    return attack;
-  });
-
-  return attack;
-};
+const check = req => new Promise((resolve, reject) => {
+  Promise.all([
+    queryChecker(req)
+  ]).then(() => resolve()).catch(() => reject())
+});
 
 module.exports = {
   checkers,
--- waf/proxy.js
+++ waf/proxy.js
@@ -11,14 +11,16 @@ const requestHandler = (req, res) => {
   logger.printPathname(req);
   logger.printQuery(req);
 
-  if(checker.check(req)){
-    res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.write('Block');
-    res.end();
-  }else{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    proxy.web(req, res, {target: rd.server});
-  }
+  checker.check(req)
+    .then(() => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      proxy.web(req, res, {target: rd.server});
+    })
+    .catch(err => {
+      res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
+      res.write('Block');
+      res.end();
+    });
 }
 
 http.createServer(requestHandler).listen(port);
